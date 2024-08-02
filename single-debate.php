0dd2494695fe4f925e511c1a51196ec702f9faf7
stnc-winDesktop <selmantunc@gmail.com>
2024-08-01 18:24:45 -0600

    video done

--- single-debate.php
+++ single-debate.php
@@ -83,23 +83,48 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
                   data-bs-parent="#accordionExample">
                   <div class="accordion-body">
-
-
-
                     <?php
                     $video_list_db = get_post_meta($debateID, 'tvsDebateMB_videoList', true);
 
                     $json_video_list = json_decode($video_list_db, true);
-                    // echo "<pre>";
-// print_r($speaker_list_json);
+
                     if ($json_video_list):
-                      echo '<ul>';
-                      foreach ($json_video_list as $key => $video) {
-                        echo '<li> <a target="_blank" href="' . $video["youtube"] . '"> <img src="' . $video["youtubePicture"] . '" alt="" width="500" height="600">    Click</a> </li>';
-                      }
-                      echo '</ul>';
-                    endif;
-                    ?>
+                      ?>
+                      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        <?php
+                        foreach ($json_video_list as $key => $video):
+                          $url = wp_get_attachment_url($video["youtubePicture"], 'thumbnail');
+                          ?>
+                          <div class="col">
+                            <div class="card shadow-sm">
+                              <a href="#inline-video<?php echo $key ?>" class="debateBox"
+                                data-glightbox="width: 700; height: auto;">
+                                <img width="200" height="200" src="<?php echo $url ?>" alt="image" />
+                              </a>
+
+                              <div id="inline-video<?php echo $key ?>" style="display: none">
+                                <div class="inline-inner">
+                                  <h4 class="text-center"><?php echo get_the_title($debateID) ?></h4>
+                                  <div class="text-center">
+
+                                    <iframe width="600" height="400"
+                                      src="https://www.youtube.com/embed/<?php echo $video["youtube_link"] ?>?autoplay=0&mute=1"
+                                      title="YouTube video player" frameborder="0"
+                                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    <p>
+                                      <?php echo $video["description"] ?>
+                                    </p>
+                                  </div>
+                                  <a class="gtrigger-close inline-close-btn" href="#">Close Box</a>
+                                </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        <?php endforeach;   ?>
+                      </div>
+                    <?php endif; ?>
 
 
                   </div>
@@ -162,22 +187,48 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
                 </div>
 
                 <div id="contact-ua" class="tab-pane active show" role="tabpanel">
-                  <?php
-                  $video_list_db = get_post_meta($debateID, 'tvsDebateMB_videoList', true);
+                <?php
+                    $video_list_db = get_post_meta($debateID, 'tvsDebateMB_videoList', true);
 
-                  $json_video_list = json_decode($video_list_db, true);
-                  // echo "<pre>";
-// print_r($speaker_list_json);
-                  if ($json_video_list):
-                    echo '<ul>';
-                    foreach ($json_video_list as $key => $video) {
+                    $json_video_list = json_decode($video_list_db, true);
 
-                      echo '<li> <a target="_blank" href="' . $video["youtube"] . '"> <img src="' . $video["youtubePicture"] . '" alt="" width="500" height="600">    Click</a> <li>';
+                    if ($json_video_list):
+                      ?>
+                      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        <?php
+                        foreach ($json_video_list as $key => $video):
+                          $url = wp_get_attachment_url($video["youtubePicture"], 'thumbnail');
+                          ?>
+                          <div class="col">
+                            <div class="card shadow-sm">
+                              <a href="#inline-video<?php echo $key ?>" class="debateBox"
+                                data-glightbox="width: 700; height: auto;">
+                                <img width="200" height="200" src="<?php echo $url ?>" alt="image" />
+                              </a>
 
-                    }
-                    echo '</ul>';
-                  endif;
-                  ?>
+                              <div id="inline-video<?php echo $key ?>" style="display: none">
+                                <div class="inline-inner">
+                                  <h4 class="text-center"><?php echo get_the_title($debateID) ?></h4>
+                                  <div class="text-center">
+
+                                    <iframe width="600" height="400"
+                                      src="https://www.youtube.com/embed/<?php echo $video["youtube_link"] ?>?autoplay=0&mute=1  "
+                                      title="YouTube video player" frameborder="0"
+                                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    <p>
+                                      <?php echo $video["description"] ?>
+                                    </p>
+                                  </div>
+                                  <a class="gtrigger-close inline-close-btn" href="#">Close Box</a>
+                                </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        <?php endforeach;   ?>
+                      </div>
+                    <?php endif; ?>
                 </div>
 
               </div>
@@ -187,4 +238,13 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
       <?php endif; ?>
     </div>
   <?php } ?>
+
+  <link rel='stylesheet' href='/wp-content/plugins/tvs-debate/assets/css/min/glightbox.min.css' media='all' />
+<script src="/wp-content/plugins/tvs-debate/assets/js/glightbox.min.js" id="jquery-mag-js"></script>
+<script>
+    var lightboxInlineIframe = GLightbox({
+      selector: '.debateBox'
+    });
+  </script>
+
   <?php get_footer(); ?>
