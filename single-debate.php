338c5805e99d6aa1e2e0ce2b1f0192232348946b
stnc-winDesktop <selmantunc@gmail.com>
2024-08-02 14:45:44 -0600

    fixed image size

--- single-debate.php
+++ single-debate.php
@@ -27,40 +27,42 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
 
 
 
-          <div class="featured-image" style="margin-bottom: 10px">
-          <?php  if (has_post_thumbnail()) :  ?>
+            <div class="featured-image" style="margin-bottom: 10px">
+              <?php if (has_post_thumbnail()): ?>
                 <?php //the_post_thumbnail();            // just the image        ?>
-                <?php //the_post_thumbnail('thumbnail'); // just the thumbnail    ?>  
-                <?php //the_post_thumbnail('medium');    // just the Medium Image ?>  
-                <?php //the_post_thumbnail('large');     // just the Medium Image ?>  
-                <?php 
-                    //      https://stackoverflow.com/questions/9305040/how-can-i-check-for-a-thumbnail-in-wordpress
-
+                <?php //the_post_thumbnail('thumbnail'); // just the thumbnail    ?>
+                <?php //the_post_thumbnail('medium');    // just the Medium Image ?>
+                <?php //the_post_thumbnail('large');     // just the Medium Image ?>
+                <?php
+                //      https://stackoverflow.com/questions/9305040/how-can-i-check-for-a-thumbnail-in-wordpress
+          
                 // adding a 200x200 height and width along with a class to it.
-                  //  the_post_thumbnail(array( 200,200 ), array( 'class' => 'alignleft' )); 
-                ?> 
-                <?php 
-                // Adding a few classes to the medium image
-                    the_post_thumbnail('large', array('class' => 'alignleft-')); 
+                //  the_post_thumbnail(array( 200,200 ), array( 'class' => 'alignleft' )); 
                 ?>
-            
-            <?php
-             else:
-              $url = wp_get_attachment_url(get_post_thumbnail_id($debateID), 'full'); ?>
-              <img src="<?php echo $url ?>" />
-              <?php   endif   ?>
-          </div>
-  
-          <div class="accordion accordion-flush------" id="accordionSingleDebate">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSpeaker" aria-expanded="false" aria-controls="flush-collapseSpeaker">
-      Speakers
-      </button>
-    </h2>
-    <div id="flush-collapseSpeaker" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionSingleDebate">
-      <div class="accordion-body">
-      <?php
+                <?php
+                // Adding a few classes to the medium image
+                the_post_thumbnail('large', array('class' => 'alignleft-'));
+                ?>
+
+                <?php
+              else:
+                $url = wp_get_attachment_url(get_post_thumbnail_id($debateID), 'full'); ?>
+                <img src="<?php echo $url ?>" />
+              <?php endif ?>
+            </div>
+
+            <div class="accordion accordion-flush------" id="accordionSingleDebate">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingOne">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseSpeaker" aria-expanded="false" aria-controls="flush-collapseSpeaker">
+                    Speakers
+                  </button>
+                </h2>
+                <div id="flush-collapseSpeaker" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                  data-bs-parent="#accordionSingleDebate">
+                  <div class="accordion-body">
+                    <?php
                     $speaker_list_db = get_post_meta($debateID, 'tvsDebateMB_speakerList', true);
 
                     $speaker_list_json = json_decode($speaker_list_db, true);
@@ -80,18 +82,20 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
                       echo '</ul>';
                     endif;
                     ?>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseVideo" aria-expanded="false" aria-controls="flush-collapseVideo">
-        Videos
-      </button>
-    </h2>
-    <div id="flush-collapseVideo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionSingleDebate">
-      <div class="accordion-body">
-      <?php
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseVideo" aria-expanded="false" aria-controls="flush-collapseVideo">
+                    Videos
+                  </button>
+                </h2>
+                <div id="flush-collapseVideo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
+                  data-bs-parent="#accordionSingleDebate">
+                  <div class="accordion-body">
+                    <?php
                     $video_list_db = get_post_meta($debateID, 'tvsDebateMB_videoList', true);
 
                     $json_video_list = json_decode($video_list_db, true);
@@ -101,19 +105,16 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
                       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                         <?php
                         foreach ($json_video_list as $key => $video):
-                          $src = wp_get_attachment_image_src(  $video["youtubePicture"], 'thumbnail', false, '' );
+                          $src = wp_get_attachment_image_src($video["youtubePicture"], 'thumbnail', false, '');
                           ?>
                           <div class="col">
                             <div class="card shadow-sm">
                               <a href="#inline-video<?php echo $key ?>" class="debateBox"
                                 data-glightbox="width: 700; height: auto;">
-                                <?php
-                                if  ( ! empty( $src ) ) :
-                                  ?>
-                                 <img   src="<?php echo $src[0] ?>" style="max-width:none!important; height: 120px !important; width: 120px !important; padding:2px" alt="image" />
-                             <?php endif  ?>
-                               
-                              </a>
+                                <?php if (!empty($src)): ?>
+                                  <img src="<?php echo $src[0] ?>"
+                                    style="max-width:none!important; height: 120px !important; width: 120px !important; padding:2px" alt="featured-image<?php echo $key ?>"/>
+                                <?php endif ?> </a>
 
                               <div id="inline-video<?php echo $key ?>" style="display: none">
                                 <div class="inline-inner">
@@ -135,16 +136,16 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
                             </div>
 
                           </div>
-                        <?php endforeach; 
-                          ?>
+                        <?php endforeach;
+                        ?>
                       </div>
                     <?php endif; ?>
 
-      </div>
-    </div>
-  </div>
+                  </div>
+                </div>
+              </div>
 
-</div>
+            </div>
 
 
           </div>
@@ -157,8 +158,8 @@ if ($builder_id && 'publish' == get_post_status($builder_id)) {
   <?php } ?>
 
   <link rel='stylesheet' href='/wp-content/plugins/tvs-debate/assets/css/min/glightbox.min.css' media='all' />
-<script src="/wp-content/plugins/tvs-debate/assets/js/glightbox.min.js" id="jquery-mag-js"></script>
-<script>
+  <script src="/wp-content/plugins/tvs-debate/assets/js/glightbox.min.js" id="jquery-mag-js"></script>
+  <script>
     var lightboxInlineIframe = GLightbox({
       selector: '.debateBox'
     });
